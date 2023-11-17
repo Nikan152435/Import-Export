f4191eb314502d33a1fe7bcd320f4df8ce606bff
NikaN <NikaN7602@gmail.com>
2023-11-17 20:06:16 +0100

    README.md

---
+++ src/domain.js
@@ -0,0 +1,3 @@
+export default class Character {
+    // код класса Character
+  }
---
+++ src/game.js
@@ -0,0 +1,19 @@
+import Character from './domain.js';
+
+class Game {
+  // код класса Game
+}
+
+class GameSavingData {
+  // код класса GameSavingData
+}
+
+function readGameSaving() {
+  // код функции readGameSaving
+}
+
+function writeGameSaving() {
+  // код функции writeGameSaving
+}
+
+export { Game as default, GameSavingData, readGameSaving, writeGameSaving, Character };
